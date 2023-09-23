5028408d2b3897dee092c91676bf0e969f062f07
GustavoPMarcena <gustavopmarcena@outlook.com>
2023-09-23 16:59:19 -0300

    fix: correções

--- Q2/script.js
+++ Q2/script.js
@@ -1,7 +1,20 @@
 let buttom = document.getElementById("buttonQ2");
 let div = document.querySelector("#DivQ2");
+let input = document.getElementById("inputTarefa");
+
+
+input.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+      addTarefa();
+    }
+  
+});
 
 buttom.addEventListener("click", () => {
+    addTarefa();
+});
+
+function addTarefa(){
     let content = document.getElementById("inputTarefa").value;
 
     const newTask = document.createElement("div");
@@ -13,8 +26,7 @@ buttom.addEventListener("click", () => {
     div.appendChild(newTask);
 
     document.getElementById("inputTarefa").value = "";
-
-});
+}
 
 function deleteTask(element) {
     const parent = element.closest("div");
